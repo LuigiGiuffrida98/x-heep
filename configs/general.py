49ef5289f0715e4112fee092f0f96e2f6474e677
Luigi Giuffrida <luigi.giuffrida.98@gmail.com>
2026-09-23 16:15:51 +0200

    Revert changes

--- configs/general.py
+++ configs/general.py
@@ -61,20 +61,20 @@ def config():
 
     system.set_debug_ss(DebugSS(has_spi_slave=1))
 
-    base_peripherals = AddressRegion(
-        "base_peripheral_domain", start_address=0x20000000, length=0x00100000
-    )
-
-    user_peripherals = AddressRegion(
-        "user_peripheral_domain", start_address=0x30000000, length=0x00100000
-    )
-
     address_map = AddressMap()
     address_map.add_region(
         AddressRegion("debug", start_address=0x10000000, length=0x00100000)
     )
-    address_map.add_region(base_peripherals)
-    address_map.add_region(user_peripherals)
+    address_map.add_region(
+        AddressRegion(
+            "base_peripheral_domain", start_address=0x20000000, length=0x00100000
+        )
+    )
+    address_map.add_region(
+        AddressRegion(
+            "user_peripheral_domain", start_address=0x30000000, length=0x00100000
+        )
+    )
     address_map.add_region(
         AddressRegion("flash_mem", start_address=0x40000000, length=0x01000000)
     )
@@ -124,8 +124,8 @@ def config():
     user_peripheral_domain.add_peripheral(UART(0x00080000))
 
     # Add the peripheral domains to the system
-    system.add_peripheral_domain(user_peripheral_domain)
     system.add_peripheral_domain(base_peripheral_domain)
+    system.add_peripheral_domain(user_peripheral_domain)
 
     interrupts = Interrupts()
     interrupts.add_interrupt("null_intr", 0)
